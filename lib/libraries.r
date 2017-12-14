f0dd8b8d4e68d28228befd53d18e84df92752a26
Ana Marija Kravanja <anamarija.kravanja@gmail.com>
2017-12-14 09:56:03 +0100

    spremembe uvoza

--- lib/libraries.r
+++ lib/libraries.r
@@ -6,7 +6,9 @@ library(gsubfn)
 library(ggplot2)
 library(reshape2)
 library(shiny)
+library(tidyr)
+library(gsubfn)
+library(XML)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
-
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,13 +1,6 @@
 # 2. faza: Uvoz podatkov, tidy data Ana Marija Kravanja
-library(knitr)
-library(dplyr)
-library(readr)
-library(rvest)
-library(gsubfn)
-library(ggplot2)
-library(reshape2)
-library(shiny)
-library(tidyr)
+
+
 sl <- locale(encoding = "Windows-1250", decimal_mark = ",", grouping_mark = ".")
 
 #uvoz prve tabele: povprečne plače po dejavnostih
@@ -22,6 +15,7 @@ stolpci <- data.frame(spol = povprecne_place_po_dejavnostih[2, ] %>% unlist(),
   fill(1:3) %>% apply(1, paste, collapse = "")
 stolpci[1] <- "dejavnost"
 colnames(povprecne_place_po_dejavnostih) <- stolpci
+
 povp.place.dejavnost <- melt(povprecne_place_po_dejavnostih[-c(1, 2), ], value.name = "povp.placa",
                              id.vars = "dejavnost", variable.name = "stolpec") %>%
   mutate(stolpec = parse_character(stolpec)) %>%
@@ -44,8 +38,9 @@ stolpci1[1] <- "statisticna regija"
 stolpci1[2] <- "starost"
 colnames(povprecne_place_po_statisticnih_regijah) <-stolpci1
 povprecne_place_po_statisticnih_regijah <- fill(povprecne_place_po_statisticnih_regijah,"statisticna regija")[-c(2,11,20,29,38,47,56,65,74,83,92,110), ]
-View(povprecne_place_po_statisticnih_regijah)
 
+povpr.place.stat.reg. <- melt(povprecne_place_po_statisticnih_regijah[-c(1), ], value.name = "povpr.placa",
+                              id.vars = "statisticna regija", variable.name = "stolpec")
 
 
 #uvoz tretje tabele: povprecne place glede na izobrazbo
@@ -57,43 +52,30 @@ stolpci2 <- data.frame(spol = povprecne_place_glede_na_izobrazbo[1,] %>% unlist(
                        leto = colnames(povprecne_place_glede_na_izobrazbo) %>%
                        { gsub("X.*", NA, .) } %>% parse_number()) %>% 
   fill(1:2) %>% apply(1, paste, collapse = "")
-stolpci1[1] <- "sektor"
-stolpci1[2] <- "spol"
-colnames(povprecne_place_glede_na_izobrazbo) <-stolpci1
+stolpci2[1] <- "sektor"
+stolpci2[2] <- "spol"
+colnames(povprecne_place_glede_na_izobrazbo) <-stolpci2
 povprecne_place_glede_na_izobrazbo <- fill(povprecne_place_glede_na_izobrazbo,"sektor")[-c(2,6,10,14,18), ]
-View(povprecne_place_glede_na_izobrazbo)
 
 #uvoz četrte tabele: minimalne_place_v_Evropi
-library(gsubfn)
-library(readr)
-library(dplyr)
-library(XML)
-library(reshape2)
-
 minimalne_place_v_evropi <- readHTMLTable("podatki/minimalne_place_v_Evropi.html",
                                       which = 1)
-colnames(minimalne_place_v_evropi) <- c("DRŽAVA", 1999:2017)
-
-for (col in colnames(minimalne_place_v_evropi)) {
- minimalne_place_v_evropi[minimalne_place_v_evropi[[col]] == ":(z)", col] <- NA}
-
-problems(minimalne_place_v_evropi)
+colnames(minimalne_place_v_evropi) <- c("DRZAVA", 1999:2017)
 View(minimalne_place_v_evropi)
 
+minimalne_place_v_evropi <- melt(minimalne_place_v_evropi, id.vars = "DRZAVA", variable.name = "leto",
+                                 value.name = "place") %>% mutate(place = parse_number(place, na = c(":", ":(z)"))) %>%
+  drop_na("place")
 
 
 #uvoz podatkov html izdatki za potovanje
-
-
 izdatki_za_potovanja <- readHTMLTable("podatki/izdatki_za_potovanje.html",
                           which = 1)
-colnames(izdatki_za_potovanja) <- c("DRŽAVA", 2012:2016)
+colnames(izdatki_za_potovanja) <- c("DRZAVA", 2012:2016)
 
-for (col in colnames(izdatki_za_potovanja)) {
-  izdatki_za_potovanja[izdatki_za_potovanja[[col]] == ":", col] <- NA}
-izdatki_za_potovanja <- gather(izdatki_za_potovanja, `2012`, `2013`, `2014`, `2015`, `2016`,key = "leta", value = "izdatki v €")
-problems(izdatki_za_potovanja)
-View(izdatki_za_potovanja)
+izdatki_za_potovanja <- melt(izdatki_za_potovanja, id.vars = "DRZAVA", variable.name = "leto",
+                             value.name = "izdatki") %>% mutate(izdatki= parse_number(izdatki, na = c(":", ":(z)"))) %>%
+  drop_na("izdatki")
 
 #združevanje tabel 4. in 5.
 
