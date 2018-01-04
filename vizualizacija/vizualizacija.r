68cd32246b41e26d52674dd7891b52a6ca2c6e0f
Ana Marija Kravanja <anamarija.kravanja@gmail.com>
2018-01-05 00:16:19 +0100

    uvozila sem zemljevid statističnih regij

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -12,16 +12,23 @@ povpr.place.izobr<-povpr.place.izobr[!(povpr.place.izobr$izobrazba=="Izobrazba -
 povpr.place.izobr <- povpr.place.izobr[- grep("SKUPAJ", povpr.place.izobr$sektor),]
 povpr.place.izobr<-povpr.place.izobr[!(povpr.place.izobr$spol=="Spol - SKUPAJ"),]
 
+
 h <- ggplot(povpr.place.izobr) + aes(x = leto, y = povpr.placa ,color=izobrazba, shape = spol) + geom_point()+ ggtitle("povprečne plače glede na izobrazbo")
 
 
 povpr.place.stat.reg.<-povpr.place.stat.reg.[(povpr.place.stat.reg.$starost=="Starost - SKUPAJ"),]
-
-
+povpr.place.stat.reg.<-povpr.place.stat.reg.[(povpr.place.stat.reg.$spol=="Spol - SKUPAJ"),]
+povpr.place.stat.reg.<-povpr.place.stat.reg.[- grep("SLOVENIJA", povpr.place.stat.reg.$regija),]
+povpr.place.stat.reg.<-povpr.place.stat.reg.[(povpr.place.stat.reg.$leto=="2016"),]
 
 zemljevid <- uvozi.zemljevid("http://www.stat.si/doc/Geo/Statisticne_regije_NUTS3.zip",
     "statisticne_regije", encoding = "Windows-1250")%>%
     pretvori.zemljevid()
+ggplot() + geom_polygon(data = zemljevid, aes(x = long, y = lat,
+                                           group = group, fill = id)) +
+  guides(fill = FALSE)
+
+
 
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
