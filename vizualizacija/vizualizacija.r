588fa3d51872caff5bafaeb20697965e1a1beecd
Ana Marija Kravanja <anamarija.kravanja@gmail.com>
2018-01-16 23:17:57 +0100

    Spremenila projekt.rmd

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -25,8 +25,6 @@ izobrazba <- povpr.place.izobr%>% filter(spol != "Spol - SKUPAJ",
                                          sektor != "1 Javni in zasebni sektor - SKUPAJ",
                                          sektor != "11 Javni sektor - SKUPAJ")
 
-izobr<- ggplot(izobrazba) + aes(x = leto, y = povpr.placa ,color=izobrazba, shape = spol) + geom_point()+ ggtitle("Povprečne plače glede na izobrazbo in spol")
-
 
 #povprečna plača glede na dejavnost
 
@@ -37,33 +35,12 @@ dejavnost <- ggplot(povp.place.dejavnost, aes(dejavnost,povprecje,fill =dejavnos
         axis.text = element_blank(),
         axis.title = element_blank(),
         axis.line = element_blank())
-  
-dejavnost +coord_polar() 
 
 
 #povprečna plača v določeni regiji 
-
 zemljevid <- uvozi.zemljevid("http://www.stat.si/doc/Geo/Statisticne_regije_NUTS3.zip",
-    "statisticne_regije", encoding = "Windows-1250")%>%
+    "Statisticne_regije", encoding = "Windows-1250")%>%
     pretvori.zemljevid()
-ggplot() + geom_polygon(data = zemljevid, aes(x = long, y = lat,
-                                           group = group, fill = id)) +
-  guides(fill = FALSE)
-
-ggplot() + geom_polygon(data = left_join(zemljevid,povpr.place.stat.reg.,
-                                         by = c("IME" = "regija")),
-                        aes(x = long, y = lat, group = group, fill = povprecje))
 
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
-                             "OB/OB", encoding = "Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels = levels(obcine$obcina))
-zemljevid <- pretvori.zemljevid(zemljevid)
-
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje = sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
 
